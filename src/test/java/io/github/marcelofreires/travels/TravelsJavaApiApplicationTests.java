9c957cb8c80ddc65b85067228e2a0c4b3497a563
marcelofreires <marcelo.freire.silva@gmail.com>
2022-01-19 23:44:29 -0300

    Init Java project

---
+++ src/test/java/io/github/marcelofreires/travels/TravelsJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.marcelofreires.travels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelsJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
